e733815208b50929d2b324123fed537d896ae01b
Arunachalam <binary2quantum001@DESKTOP-RT9G0OP.B2Q.local>
2020-04-24 21:55:05 +0530

    whatsapp and categoy 24/4/2020

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../../webservice/category/category.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
+   public category=[];
+   public subcategory=[];
+  errorMsg: any;
+  constructor(private _category:CategoryService) { }
+   
   ngOnInit() {
+     this._category.getcategory().subscribe(res=>this.category=res.data,
+      error => this.errorMsg = error);
+     this._category.getsubcategory().subscribe(res=>this.subcategory=res.data ,
+      error => this.errorMsg = error);
   }
 
 }
